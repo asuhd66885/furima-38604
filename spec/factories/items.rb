bcaeef834046890612d50effa5587d9f7df1cb05
asuhd66885 <ray3bow1@gmail.com>
2022-12-02 22:18:13 +0900

    商品出品機能　修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Lorem.sentence }
-    image { Faker::Lorem.sentence }
     info { Faker::Lorem.sentence }
     association :user
     category_id { 2 }
