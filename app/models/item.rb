990260755dfbe6515f7370159352862f4e4ef78e
asuhd66885 <ray3bow1@gmail.com>
2022-12-02 21:18:39 +0900

    商品出品機能　完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,11 @@ class Item < ApplicationRecord
     validates :info
     validates :image
   end
-  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
-  with_options presence: true, numericality: { other_than: 1, message: 'Select'} do
+  with_options presence: true, numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
     validates :sales_status_id
     validates :shipping_fee_status_id
--- app/models/shipping_fee_status.rb
+++ app/models/shipping_fee_status.rb
@@ -6,4 +6,4 @@ class ShippingFeeStatus < ActiveHash::Base
   ]
   include ActiveHash::Associations
   has_many :items
-  end
+end
