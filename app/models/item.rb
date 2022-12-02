695f59e5d5179c41ce8bc4d5e3ad24bc07684cb1
asuhd66885 <ray3bow1@gmail.com>
2022-12-02 11:39:40 +0900

    商品出品ページに遷移完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :image, presence: true
+  validates :name,        presence: true
+  validates :nickname,        presence: true
+  validates :info,        presence: true
+  validates :nickname,        presence: true
+  validates :category_id ,        presence: true
+  validates :sales_status_id,        presence: true
+  validates :shipping_fee_status_id,        presence: true
+  validates :prefecture_id,        presence: true
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, presence: true
 end
