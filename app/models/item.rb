e5b3e8634ca3a12634fce5425eae1805b8216698
asuhd66885 <ray3bow1@gmail.com>
2022-12-03 15:34:21 +0900

    商品出品機能　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
     validates :info
     validates :image
   end
-  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price, presence: true, numericality: { only_integer: true, message: 'Half-width number' }
   validates :price,
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,10 +10,6 @@ RSpec.describe Item, type: :model do
       it '必須項目が全てあれば登録できること' do
         expect(@item).to be_valid
       end
-      it 'ログイン状態のユーザーのみ、商品出品ページへ遷移できること' do
-        @item = FactoryBot.create(:user)
-        expect(@item).to be_valid
-      end
     end
 
     context '出品ができない時' do
